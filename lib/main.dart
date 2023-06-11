6682bfbd67fe8f06b5438e4266a042e42a5550d4
mbanginujam <jambernardmbanginu@gmail.com>
2023-06-11 12:28:59 +0100

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -39,28 +39,114 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.fromLTRB(0, 70, 0, 0),
+          margin: EdgeInsets.all(2.75),
+          decoration: BoxDecoration(
+            color: Colors.purple[900],
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(
+                  0,
+                  3,
+                ),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(
+                      left: 45, right: 55, top: 41.34, bottom: 29),
+                ),
+                Image.asset(
+                  'assets/images/screen_ilus.jpeg',
+                  height: 200,
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Text(
+                  'Jot Down anything you want to achieve, today or in the futere',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      color: Colors.yellow,
+                      iconSize: 25.0,
+                      splashColor: Colors.blue,
+                      onPressed: () {},
+                      icon: Icon(Icons.circle_sharp),
+                    ),
+                    IconButton(
+                      color: Colors.white70,
+                      iconSize: 25.0,
+                      splashColor: Colors.blue,
+                      onPressed: () {},
+                      icon: Icon(Icons.circle_sharp),
+                    ),
+                    IconButton(
+                      color: Colors.white,
+                      iconSize: 25.0,
+                      splashColor: Colors.blue,
+                      onPressed: () {},
+                      icon: Icon(Icons.circle_sharp),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.all(28),
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Lets Get Started',
+                        style: TextStyle(
+                          color: Colors.purple[900],
+                          fontSize: 17,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      IconButton(
+                        color: Colors.purple[900],
+                        iconSize: 25,
+                        splashColor: Colors.blue,
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_forward_rounded),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
